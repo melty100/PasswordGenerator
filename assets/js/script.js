dad8d0e25ff8e751bc335fb5120a2d6f72f2d47a
Connor <melty100@gmail.com>
2020-07-21 12:15:19 -0700

    Added comments to index and removed console logs statements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,74 +1,104 @@
+/********************************
+ 
+   Random Password Generator
+   Author: Connor Melton
+   Date: 07/20/2020 
 
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var specialChar = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
-var numericChar = "1234567890";
+*********************************/
 
-function gp() {
 
-    alert("called generate password function");
-    var includeUpperCase = false;
-    var includeLowerCase = false;
-    var includeSpecialChar = false;
-    var includeNumeric = false;
-    var length = 0;
+//Character subsets for user specified password content
+const UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
+const SPECIAL_CHARS = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+const NUMERIC_CHARS = "1234567890";
 
-    length = getLength();
-    includeUpperCase = confirm("Does you password need upper case letters?");
-    includeLowerCase = confirm("Does your password need lower case letters?");
-    includeSpecialChar = confirm("Does your passowrd need special characters?");
-    includeNumeric = confirm("Does your password need to contain numbers?");
+//Valid password lengths
+const MIN_LENGTH = 8;
+const MAX_LENGTH = 128;
 
-    var password = createPassword(includeUpperCase, includeLowerCase, includeSpecialChar, includeNumeric, length);
+// Generates a random password and sets textarea with id 'password' equal to it
+// Input: None
+// Return: None
+function generatePassword() {
 
-    document.getElementById("password-field").innerHTML = password;
+    alert("Beginning password generation");
+
+    //Prompts user for password length
+    let length = getLength();
+
+    //Generates password with user specifications
+    let password = createPassword(length);
+
+    //Changes textarea content to randomly generated password
+    document.querySelector("#password").textContent = password;
 
 }
 
+// Prompts user for password length and checks for valid input (8 >= x >= 128).
+// Input : None
+// Return : Number
 function getLength() {
 
-    var length = parseInt(prompt("Enter a number between 8 and 128 for password length."));
+    let length = parseInt(prompt("Enter a number between 8 and 128 for password length."));
 
-    while (isNaN(length) || (length < 8 || length > 128)) {
-
-        length = parseInt(prompt("Invalid Input Error - Enter a number between 8 and 128 for password length."));
+    //Checks for a valid length from user
+    while (isNaN(length) || (length < MIN_LENGTH || length > MAX_LENGTH)) {
+        length = parseInt(prompt("Error - Invalid Input : Enter a number between 8 and 128 for password length."));
     }
 
     return length;
 }
 
-function createPassword(upper, lower, special, numeric, passwordLength) {
+// Returns a randomly generated password that follows user specifications
+// Input : Number
+// Return : String
+function createPassword(passwordLength) {
 
-    var validChars = "";
-    var password = "";
+    //User specified character types
+    let includeUpperCase = false;
+    let includeLowerCase = false;
+    let includeSpecialChar = false;
+    let includeNumericChar = false;
 
-    if(upper){
-        validChars += upperCase;
-    }
-    if(lower){
-        validChars += lowerCase;
-    }
-    if(special){
-        validChars += specialChar;
-    }
-    if(numeric){
-        validChars += numericChar;
-    }
+    //String of user desired characters for password generation
+    let validChars = "";
 
-    console.log(validChars);
+    //Return value
+    let password = "";
 
-    var validCharsLength = validChars.length;
+    //Checks for at least one character type selection from user
+    while (!(includeNumericChar || includeSpecialChar || includeLowerCase || includeUpperCase)) {
 
-    for(x = 0; x < passwordLength; x++){
+        alert("Choose at least one of the four following character types for password generation");
 
-        //Reports randomly generated index to console for testing
-        let index = Math.floor(Math.random() * (validCharsLength - 1))
-
-        console.log(index);
-
-        password += validChars[index];
+        includeUpperCase = confirm("Does you password need upper case letters?");
+        includeLowerCase = confirm("Does your password need lower case letters?");
+        includeSpecialChar = confirm("Does your password need special characters?");
+        includeNumericChar = confirm("Does your password need to contain numbers?");
     }
 
-    console.log(password);
+    //Valid character set formation
+    if (includeUpperCase) { validChars += UPPERCASE; }
+    if (includeLowerCase) { validChars += LOWERCASE; }
+    if (includeSpecialChar) { validChars += SPECIAL_CHARS; }
+    if (includeNumericChar) { validChars += NUMERIC_CHARS; }
+
+    //Finds length of valid char set for random character selection
+    let validCharsLength = validChars.length;
+
+    //Password formation
+    for (x = 0; x < passwordLength; x++) {
+
+        let i = Math.floor(Math.random() * (validCharsLength - 1))
+        password += validChars[i];
+    }
+
     return password;
 }
+
+//Gets reference to button with id=generate
+var generateBtn = document.querySelector("#generate");
+
+//Calls generatePassword() when generate button is clicked
+generateBtn.addEventListener("click", generatePassword);
